21dbdef86551278ec8976032841f6ac7e808b3c9
GithubofKillian <killian.jonneaux@gmail.com>
2024-05-17 14:15:42 +0200

    Creation fxml Client part
    Navigation through buttons availaible on the Client part, we can go to Sign Up or Clients Research interface

--- src/main/java/group/projetcybooks/SceneController.java
+++ src/main/java/group/projetcybooks/SceneController.java
@@ -13,6 +13,10 @@ public class SceneController {
     private Stage stage;
     private Scene scene;
 
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
     public void switchToMain(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("MainScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -71,6 +75,21 @@ public class SceneController {
 
     public void switchToReturnBook2(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("ReturnBook2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToClientsResearch(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("ClientsResearch.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToClientsSignUp(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("ClientsSignUp.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
