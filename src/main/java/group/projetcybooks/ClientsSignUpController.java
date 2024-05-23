9c08ede1802bcc4d39f4f58d904b8699e0cbf176
GithubofKillian <killian.jonneaux@gmail.com>
2024-05-23 13:59:09 +0200

    Link Sign Up Interface with Methode

--- src/main/java/group/projetcybooks/ClientsSignUpController.java
+++ src/main/java/group/projetcybooks/ClientsSignUpController.java
@@ -5,15 +5,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import group.projetcybooks.SceneController;
+import group.projetcybooks.client.Client;
 
 import java.io.IOException;
 
 public class ClientsSignUpController extends SceneController{
 
+    public TextField lastNameField;
+    public TextField firstNameField;
+
+    public TextField phoneField;
     private Stage stage;
     private Scene scene;
 
     public void setStage(Stage stage) {
         this.stage = stage;
+    }
+
+    public void handleSignUp() {
+        String lastName = lastNameField.getText();
+        String firstName = firstNameField.getText();
+        String phone = phoneField.getText();
+
+        int result = new Client().clientSendNewUser(lastName,firstName,phone);
+
+        if (result == 1) {
+            SceneController.showError("Success", "User Registered");
+        } else {
+            SceneController.showError("Error", "Failed to register user");
+        }
     }
 }
