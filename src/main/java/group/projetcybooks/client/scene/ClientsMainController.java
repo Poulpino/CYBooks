fabeab0387bd9833819d6820620299d96bd2fe29
matiaskis <104195775+matiaskis@users.noreply.github.com>
2024-05-26 18:44:24 +0200

    Update ClientsMainController.java

--- src/main/java/group/projetcybooks/client/scene/ClientsMainController.java
+++ src/main/java/group/projetcybooks/client/scene/ClientsMainController.java
@@ -3,13 +3,21 @@ package group.projetcybooks.client.scene;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ClientsMainController extends SceneController{
+/**
+ * Controller class for the ClientsMain scene.
+ * This class handles the main interactions for the client's main view.
+ */
+public class ClientsMainController extends SceneController {
 
     private Stage stage;
     private Scene scene;
 
+    /**
+     * Sets the stage for this controller.
+     *
+     * @param stage the stage to set
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-
 }
