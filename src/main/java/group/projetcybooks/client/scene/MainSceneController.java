fb133e91708cb4c202d6bf05a11f8fad58639b89
matiaskis <104195775+matiaskis@users.noreply.github.com>
2024-05-26 18:53:05 +0200

    Update MainSceneController.java

--- src/main/java/group/projetcybooks/client/scene/MainSceneController.java
+++ src/main/java/group/projetcybooks/client/scene/MainSceneController.java
@@ -3,12 +3,21 @@ package group.projetcybooks.client.scene;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class MainSceneController extends SceneController{
+/**
+ * Controller class for the MainScene.
+ * This class handles the main interactions for the main scene of the application.
+ */
+public class MainSceneController extends SceneController {
 
     private Scene scene;
-
     private Stage stage;
 
-    public void setStage(Stage stage) { this.stage = stage; }
+    /**
+     * Sets the stage for this controller.
+     *
+     * @param stage the stage to set
+     */
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 }
