9dc6d326176d3972bf83c6aaf59f6b87e7c0de34
matiaskis <104195775+matiaskis@users.noreply.github.com>
2024-05-26 18:45:01 +0200

    Update BorrowMainController.java

--- src/main/java/group/projetcybooks/client/scene/BorrowMainController.java
+++ src/main/java/group/projetcybooks/client/scene/BorrowMainController.java
@@ -3,13 +3,21 @@ package group.projetcybooks.client.scene;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Controller class for the BorrowMain scene.
+ * This class handles the main interactions for the borrower's main view.
+ */
 public class BorrowMainController extends SceneController {
 
     private Stage stage;
     private Scene scene;
 
+    /**
+     * Sets the stage for this controller.
+     *
+     * @param stage the stage to set
+     */
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-
 }
