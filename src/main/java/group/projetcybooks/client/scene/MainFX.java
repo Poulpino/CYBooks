1051baa9062ccf188a25f766c552626f0a84000c
matiaskis <104195775+matiaskis@users.noreply.github.com>
2024-05-26 18:50:20 +0200

    Update MainFX.java

--- src/main/java/group/projetcybooks/client/scene/MainFX.java
+++ src/main/java/group/projetcybooks/client/scene/MainFX.java
@@ -8,7 +8,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * Main class for the JavaFX client application.
+ * This class starts the JavaFX application and initializes the main scene.
+ */
 public class MainFX extends Application {
+
+    /**
+     * Starts the JavaFX application by loading the main scene.
+     *
+     * @param stage the primary stage for the JavaFX application
+     * @throws IOException if an I/O error occurs during loading the FXML file
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainFX.class.getResource("MainScene.fxml"));
@@ -17,11 +28,16 @@ public class MainFX extends Application {
         stage.setScene(scene);
         stage.setFullScreen(true);
         stage.show();
-
     }
 
+    /**
+     * Main method to launch the JavaFX application.
+     * Starts the server in a separate thread and launches the JavaFX application.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
+        // Start the server in a separate thread
         new Thread(() -> {
             try {
                 Server.main(args);
@@ -30,8 +46,7 @@ public class MainFX extends Application {
             }
         }).start();
 
-        // Démarrez l'application JavaFX
+        // Launch the JavaFX application
         launch();
     }
 }
-
